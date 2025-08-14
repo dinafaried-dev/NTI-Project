839f14f5a6adc7bf4366ac747aaece3a8963d730
dinafaried-dev <dina.faried@ejust.edu.eg>
2025-08-14 20:53:32 +0300

    Update dmk3.py

--- dmk3.py
+++ dmk3.py
@@ -49,7 +49,7 @@ import pandas as pd
 import streamlit as st
 
 # قراءة الملف من فولدر data
-data = pd.read_csv("data/credit_card_churn.csv",
+data = pd.read_csv("credit_card_churn.csv",
                    usecols=lambda column: column not in [
                        'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1',
                        'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2'
